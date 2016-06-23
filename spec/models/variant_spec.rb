a2e77d162c3274172df0bce0085f6b7b6fa5832e
Chris Blackburn <christopher.blackburn@gmail.com>
2016-06-23 20:10:27 +0100

    Prove it's possible to update pre-sale prices on variant

--- spec/models/variant_spec.rb
+++ spec/models/variant_spec.rb
@@ -8,5 +8,11 @@ describe Spree::Variant do
     it 'returns the price in the provided currency' do
       expect(variant.pre_sale_price_in('GBP').amount.to_s).to eq('200.0')
     end
+
+    it 'allows you to create new pre-sale prices' do
+      pre_sale_price = variant.pre_sale_price_in('USD')
+      pre_sale_price.amount = 100
+      expect(pre_sale_price.amount.to_s).to eq('100.0')
+    end
   end
 end
