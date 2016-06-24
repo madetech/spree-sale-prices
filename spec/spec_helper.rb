ee423eebec66219c1f2d6e18ccaadb5088d3d441
Seb Ashton <seb@sebashton.com>
2016-06-24 14:25:51 +0100

    Add DatabaseCleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 require_relative 'support/rails'
+require 'database_cleaner'
+
 Dir[File.join(File.dirname(__FILE__), 'support/*.rb')].each {|f| require f }
 
 RSpec.configure do |config|
@@ -6,4 +8,18 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include Spree::TestingSupport::ControllerRequests
+
+  config.before :suite do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.before :each do
+    DatabaseCleaner.strategy = RSpec.current_example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after :each do
+    DatabaseCleaner.clean
+  end
 end
