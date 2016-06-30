1598ca4117184bcecb178fd5344ab6f2c5b07506
Chris Blackburn <christopher.blackburn@gmail.com>
2016-06-30 11:02:54 +0100

    Allow us to call on_sale? on a variant

--- app/models/concerns/pre_sale_prices/variant_pre_saleable.rb
+++ app/models/concerns/pre_sale_prices/variant_pre_saleable.rb
@@ -7,6 +7,8 @@ module PreSalePrices
                class_name: 'Spree::PreSalePrice',
                dependent: :destroy,
                inverse_of: :variant
+
+      delegate :on_sale?, to: :master
     end
 
     def pre_sale_price_in(currency)
